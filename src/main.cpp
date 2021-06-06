c6247faa737f3bc778b5a9b29a8e4ac240e1cc45
Vijay Samula <samulavijay@gmail.com>
2021-06-06 19:25:59 +0200

    Updated map file

--- src/main.cpp
+++ src/main.cpp
@@ -52,22 +52,23 @@ int main(int argc, const char **argv)
             osm_data = std::move(*data);
     }
     
-    // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
-    // user input for these values using std::cin. Pass the user input to the
-    // RoutePlanner object below in place of 10, 10, 90, 90.
-
-   float start_x, start_y, end_x, end_y;
-   
+  
+     std::cout << "Enter the values between 0 and 100"<< std::endl;
+    float start_x, start_y, end_x, end_y;
+   std::cout << "Enter origin x-coordinate "<< std::endl;
     std::cin>>start_x;
+    std::cout << "Enter origin y-coordinate "<< std::endl;
     std::cin>>start_y;
+    std::cout << "Enter destination x-coordinate "<< std::endl;
     std::cin>>end_x;
+    std::cout << "Enter destination y-coordinate "<< std::endl;
     std::cin>>end_y;
 
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
